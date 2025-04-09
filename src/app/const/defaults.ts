aa18cd10b29392f5c1828e029a17a7f9aa2e7e50
Cookies <38864138+Cookzz@users.noreply.github.com>
2025-04-10 01:05:56 +0800

    chore: cleanup, fix jquery import

--- src/app/const/defaults.ts
+++ src/app/const/defaults.ts
@@ -1,23 +1,23 @@
 import { Defaults } from "../types/default.interface";
 
 export const defaultValue: Defaults = {
-    file: { input: null },
-    probe: { duration: 0, rate: 0 },
-    dimensions: {
-      original_width: 320,
-      original_height: 240,
-      width: 320,
-      height: 240,
-      lock: true,
-      ratio: 240/320
-    },
-    fps: 24,
-    color: {
-      stats_mode: "diff",
-      diff_mode: "rectangle",
-      dither: true,
-      count: 256,
-      dither_scale: 3,
-      alpha: false
-    }
+  file: { input: null },
+  probe: { duration: 0, rate: 0 },
+  dimensions: {
+    original_width: 320,
+    original_height: 240,
+    width: 320,
+    height: 240,
+    lock: true,
+    ratio: 240/320
+  },
+  fps: 24,
+  color: {
+    stats_mode: "diff",
+    diff_mode: "rectangle",
+    dither: true,
+    count: 256,
+    dither_scale: 3,
+    alpha: false
   }
+}
