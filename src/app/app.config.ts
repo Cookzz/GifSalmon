65c20d6f080113180f1ba295ef4a1b709085c07b
Cookies <38864138+Cookzz@users.noreply.github.com>
2025-04-15 02:36:47 +0800

    chore: remove unused routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,4 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
@@ -8,7 +6,6 @@ import Aura from '@primeng/themes/aura';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
